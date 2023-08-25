90d30c503ad27b411eb85d510051dbe99d44640e
Santiago Chaves <santichavesg@gmail.com>
2023-08-25 08:47:03 -0600

    cambios en parametros

--- metodosDeOperacion.h
+++ metodosDeOperacion.h
@@ -2,21 +2,21 @@
 METODOS EN LAS NOTICIAS:
 
 
-PASAR NOTICIA AL FRENTE (ANNADIR LUEGO DEL CURSOR)                                             titular toFront(noticia);
+PASAR NOTICIA AL FRENTE (ANNADIR LUEGO DEL CURSOR)                                             titular toFront(titular noticia);
 
-BORRAR NOTICIA (REMOVE())                                                                      titular delete(noticia);
+BORRAR NOTICIA (REMOVE())                                                                      titular delete(titular noticia);
 
-BUSCAR PALABRAS (ITERATE() Y BUSCAR AHI)                                                       titular searchWord(word);
+BUSCAR PALABRAS (ITERATE() Y BUSCAR AHI)                                                       titular searchWord(string word);
 
 ENSENNAR POR RELEVANCIA (ORDENANDO POR FECHA, CON SEARCHWORD() TOFRONT() ENTRE OTROS)          titular displayRelevance();
 
-CREAR LISTA PARA INPUT (ARRAY SIMPLE, string input[n] = {"1","2","3","4",...,"n"})             titular inputRelevance(array);
+CREAR LISTA PARA INPUT (ARRAY SIMPLE, string input[n] = {"1","2","3","4",...,"n"})             titular inputRelevance(array input);
 
-RELEVANCIA POR INPUT( SIMILAR A SEARCHWORD() PERO SOLO LAS PALABRAS DEL INPUT)                 titular searchInput(array);
+RELEVANCIA POR INPUT( SIMILAR A SEARCHWORD() PERO SOLO LAS PALABRAS DEL INPUT)                 titular searchInput(array input);
 
-ELIMINAR POR INPUT( SIMILAR A SEARCHINPUT() PERO LO QUE HACE ES BORRAR NOTICIAS)               titular deleteInput(array);
+ELIMINAR POR INPUT( SIMILAR A SEARCHINPUT() PERO LO QUE HACE ES BORRAR NOTICIAS)               titular deleteInput(array input);
 
-REUBICAR NOTICIA(USANDO COUNTNODES() Y SHOWPOSITIONS() ENCUENTRA LAS POSICIONES                titular reubicate(n, +-m);
+REUBICAR NOTICIA(USANDO COUNTNODES() Y SHOWPOSITIONS() ENCUENTRA LAS POSICIONES                titular reubicate(int n, string+-m);
 PARA LUEGO HACIENDO ADVANCE PODER ANNADIR LA NOTICIA EN SU NUEVA POSICION )
 
 
