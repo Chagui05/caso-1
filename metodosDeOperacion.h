de382a8cd61686f3aa6005aefda1880f1cbde584
Santiago Chaves <santichavesg@gmail.com>
2023-08-24 21:36:17 -0600

    agregando entradas a los metodos de operacion

--- metodosDeOperacion.h
+++ metodosDeOperacion.h
@@ -2,21 +2,21 @@
 METODOS EN LAS NOTICIAS:
 
 
-PASAR NOTICIA AL FRENTE (ANNADIR LUEGO DEL CURSOR)                                             titular toFront();
+PASAR NOTICIA AL FRENTE (ANNADIR LUEGO DEL CURSOR)                                             titular toFront(noticia);
 
-BORRAR NOTICIA (REMOVE())                                                                      titular delete();
+BORRAR NOTICIA (REMOVE())                                                                      titular delete(noticia);
 
-BUSCAR PALABRAS (ITERATE() Y BUSCAR AHI)                                                       titular searchWord();
+BUSCAR PALABRAS (ITERATE() Y BUSCAR AHI)                                                       titular searchWord(word);
 
-ENSENNAR POR RELEVANCIA (ORDENANDO POR FECHA, CON SEARCHWORD() TOFRONT() ENTRE OTROS)          titular displayRelevance());
+ENSENNAR POR RELEVANCIA (ORDENANDO POR FECHA, CON SEARCHWORD() TOFRONT() ENTRE OTROS)          titular displayRelevance();
 
-CREAR LISTA PARA INPUT (ARRAY SIMPLE, string input[n] = {"1","2","3","4",...,"n"})             titular inputRelevance();
+CREAR LISTA PARA INPUT (ARRAY SIMPLE, string input[n] = {"1","2","3","4",...,"n"})             titular inputRelevance(array);
 
-RELEVANCIA POR INPUT( SIMILAR A SEARCHWORD() PERO SOLO LAS PALABRAS DEL INPUT)                 titular searchInput();
+RELEVANCIA POR INPUT( SIMILAR A SEARCHWORD() PERO SOLO LAS PALABRAS DEL INPUT)                 titular searchInput(array);
 
-ELIMINAR POR INPUT( SIMILAR A SEARCHINPUT() PERO LO QUE HACE ES BORRAR NOTICIAS)               titular deleteInput();
+ELIMINAR POR INPUT( SIMILAR A SEARCHINPUT() PERO LO QUE HACE ES BORRAR NOTICIAS)               titular deleteInput(array);
 
-REUBICAR NOTICIA(USANDO COUNTNODES() Y SHOWPOSITIONS() ENCUENTRA LAS POSICIONES                titular reubicate();
+REUBICAR NOTICIA(USANDO COUNTNODES() Y SHOWPOSITIONS() ENCUENTRA LAS POSICIONES                titular reubicate(n, +-m);
 PARA LUEGO HACIENDO ADVANCE PODER ANNADIR LA NOTICIA EN SU NUEVA POSICION )
 
 
