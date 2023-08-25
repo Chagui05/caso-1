d1e627375dc8b55a82dd9e5fac4b8c56e13076f9
Santiago Chaves <santichavesg@gmail.com>
2023-08-24 21:18:24 -0600

    annadiendo archivos para caso 1

---
+++ metodosDeOperacion.h
@@ -0,0 +1,23 @@
+/*
+METODOS EN LAS NOTICIAS:
+
+
+PASAR NOTICIA AL FRENTE (ANNADIR LUEGO DEL CURSOR)                                             titular toFront();
+
+BORRAR NOTICIA (REMOVE())                                                                      titular delete();
+
+BUSCAR PALABRAS (ITERATE() Y BUSCAR AHI)                                                       titular searchWord();
+
+ENSENNAR POR RELEVANCIA (ORDENANDO POR FECHA, CON SEARCHWORD() TOFRONT() ENTRE OTROS)          titular displayRelevance());
+
+CREAR LISTA PARA INPUT (ARRAY SIMPLE, string input[n] = {"1","2","3","4",...,"n"})             titular inputRelevance();
+
+RELEVANCIA POR INPUT( SIMILAR A SEARCHWORD() PERO SOLO LAS PALABRAS DEL INPUT)                 titular searchInput();
+
+ELIMINAR POR INPUT( SIMILAR A SEARCHINPUT() PERO LO QUE HACE ES BORRAR NOTICIAS)               titular deleteInput();
+
+REUBICAR NOTICIA(USANDO COUNTNODES() Y SHOWPOSITIONS() ENCUENTRA LAS POSICIONES                titular reubicate();
+PARA LUEGO HACIENDO ADVANCE PODER ANNADIR LA NOTICIA EN SU NUEVA POSICION )
+
+
+*/
---
+++ metodosTAD.h
@@ -0,0 +1,35 @@
+//USARE UNA LISTA CIRCULAR ENLAZADA
+/*METODOS TAD:
+
+
+CONSTRUCTOR:                           CircleList();
+
+DESTRUCTOR:                           ˜CircleList();
+
+VERIFICAR SI LA LISTA ESTA VACIA:      bool empty() const;
+
+ELEMENTO LUEGO DEL CURSOR:             const Elem& front() const; 
+
+ELEMENTO DEL CURSOR:                   const Elem& back() const; 
+
+AVANZAR CURSOR:                        void advance();
+
+ANNADIR LUEGO DEL CURSOR:              void add(const Elem& e); 
+
+BORRAR LUEGO DEL CURSOR                void remove();
+
+CONTAR NODOS:                          int countNodes();
+
+BUSCAR UN NODO:                        node searchNode():
+
+RECORRER LISTA:                        void iterate();
+
+MOSTRAR LISTA(similar a iterate()):    void display();
+
+MOSTRAR POSICIONES(CUENTA CON          void showPositions();
+countNodes  para ensennar en que 
+posicion esta cada nodo)
+
+*/
+
+
