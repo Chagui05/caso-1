737c060c25e4f8a1fe35910ddb73e82bf7e8c987
Santiago Chaves <santichavesg@gmail.com>
2023-08-28 23:35:54 -0600

    desarrollo de los metodos menos: iterate, clear, searchnode, showpositions

---
+++ main.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+//para crear nodos
+class Node{
+private:
+string elem;
+Node* next;
+friend class circularList;
+};
+
+class circularList{  
+private:
+Node* cursor;
+
+public:
+    circularList() {
+        cursor = NULL;
+    }
+    ~circularList() {             
+        while (!empty()) remove(); 
+    }
+    bool empty() const{
+        return cursor == NULL;
+    }
+    const string& back() const{        // los string posiblemente cambien por si los archivos que nos pase el profe son clases 
+        return cursor->elem; 
+    }
+    const string& front() const{
+        return cursor->next->elem;
+    }
+    void advance(){
+        cursor = cursor->next;
+    }
+    void add(const string& elem){
+        Node* aux = new Node;
+        aux->elem = elem;
+        if(cursor == NULL){
+            aux->next = aux;
+            cursor = aux;
+        }
+        else{
+            aux->next = cursor->next;
+            cursor->next = aux;
+        }
+    }
+    void remove(){
+        Node* old = cursor->next;
+        if (old == cursor){
+        cursor == NULL;
+        }
+        else{ 
+        cursor->next = old->next;
+        }
+        delete old;
+    }
+    int countNodes(){
+        Node* aux = cursor;
+        int count = 0;
+
+        if(aux != NULL){
+            count++;
+            aux = aux->next;
+        }
+        while(aux!=cursor){
+            count++;
+            aux = aux->next;
+        }
+        cout << count << endl;
+        return count;
+    }
+
+    void display() {
+        Node* aux = cursor;
+        if(aux == NULL){
+            cout << "**Empty list**" <<endl;
+            return;
+        }
+        else{
+            cout << aux->elem << endl;
+            aux = aux->next;
+            while(aux != cursor){
+                cout << aux->elem << endl;
+                aux = aux->next;
+            }  
+        }
+    }
+};
+
+int main ()
+{
+    circularList lista;
+    lista.add("hola");
+    lista.add("a");
+    lista.add("b");
+    lista.add("c");
+    lista.add("d");
+    lista.countNodes();
+    lista.display();
+
+    }
